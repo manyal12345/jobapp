fc77627e8cfc5bdf39a506e6e1179240ce7ebdad
Pramod Singh Manyal <manyalp12345@gmail.com>
2023-10-25 14:53:16 +0545

    Configuration for vercel

--- build.sh
+++ build.sh
@@ -3,11 +3,11 @@
 #Build the project
 
 echo "Building project..."
-python3.12 -m pip install -r requirements.txt
+python -m pip install -r requirements.txt
 
 echo "makemigrations"
-python3.12 manage.py makemigrations --noinput
-python3.12 manage.py migrate --noinput
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
 
 echo "collectstatic"
-python3.12 manage.py collectstatic --noinput  --clear
+python manage.py collectstatic --noinput  --clear
